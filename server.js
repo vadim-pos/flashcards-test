f112b6ae90f9e32d85a89b6628dd4e83f508f5bd
Vadim Postanovkin <v.postanovkin@gmail.com>
2017-05-22 14:43:13 +0300

    fix server.js

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const path = require('path');
 const app = express();
 
 // If an incoming request uses a protocol other than HTTPS, redirect that request to the same url but with HTTPS
-const forceSSL = function() {
-    return function (req, res, next) {
+const forceSSL = () => {
+    return (req, res, next) => {
         if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
+            return res.redirect( ['https://', req.get('Host'), req.url].join('') );
         }
         next();
     }
 }
 
-app.get('/*', function(req, res) {
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+
 // Instruct the app to use the forceSSL middleware
 app.use(forceSSL());
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
