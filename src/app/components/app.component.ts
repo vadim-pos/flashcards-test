15b8efb8175943a8b9b2ec8f62a17111eeda7a50
Vadim Postanovkin <v.postanovkin@gmail.com>
2017-05-28 14:20:04 +0300

    Update auth functionality & styles

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FirebaseService } from '../services/firebase.service';
+
 @Component({
     selector: 'app-root',
     template: `
         <app-decks [class.active]="showDecks"></app-decks>
         <main class="main">
             <header class="app-header">
-                <button (click)="showDecks = !showDecks" [class.active]="showDecks" class="decks-trigger" [title]="showDecks ? 'hide decks' : 'show decks'"></button>
+                <button *ngIf="userIsAuthenticated" (click)="showDecks = !showDecks" [class.active]="showDecks" class="decks-trigger" [title]="showDecks ? 'hide decks' : 'show decks'"></button>
                 <h1 class="app-title">
                     <a [routerLink]="['']" data-hover="Flashcards repetitor" class="app-title-link">Flashcards repetitor</a>
                 </h1>
@@ -17,11 +19,21 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
-    showDecks:boolean = true;
+    showDecks:boolean = false;
+    userIsAuthenticated:boolean = false;
 
-    constructor() {}
+    constructor(private firebaseService:FirebaseService) {}
     
     ngOnInit() {
+        this.firebaseService.authStateChanged.subscribe(userIsAuthenticated => {
+            if (userIsAuthenticated) {
+                this.showDecks = true;
+                this.userIsAuthenticated = true;
+            } else {
+                this.showDecks = false;
+                this.userIsAuthenticated = false;
+            }
+        });
         
     }
 }
