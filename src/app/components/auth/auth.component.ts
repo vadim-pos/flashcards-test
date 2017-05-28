1839ffb30d5875978ea68b672789809827e18fb8
vadim-pos <v.postanovkin@gmail.com>
2017-05-28 09:49:37 +0300

    Update forms animations

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -9,7 +9,30 @@ import { FlashcardsService } from '../../services/flashcards.service';
 @Component({
     selector: 'app-auth',
     template: `
-        <div class="auth-form sign-up">
+        
+        <div [class.active]="authMode === 'sign in'" [class.inactive]="authMode !== 'sign in'" class="auth-form sign-in">
+            <p class="form-title">Sign In</p>
+            <form #SignInForm="ngForm" (ngSubmit)="onSignIn(SignInForm)">
+                <div class="form-group">
+                    <input #signInEmail="ngModel" ngModel email required id="sign-in-email" class="form-input" name="email" type="email"/>
+                    <label [class.active]="!!signInEmail.value" class="input-field-label email" for="sign-in-email"></label>
+                    <small [class.active]="signInEmail.invalid && signInEmail.touched" class="input-error">valid email is required</small>
+                </div>
+                <div class="form-group">
+                    <input #signInPwd="ngModel" ngModel required minlength="6" id="sign-in-pwd" class="form-input" name="password" type="password"/>
+                    <label [class.active]="!!signInPwd.value" class="input-field-label password" for="sign-in-pwd"></label>
+                    <small [class.active]="signInPwd.invalid && signInPwd.touched" class="input-error">6 characters password is required</small>
+                </div>
+                <div *ngIf="signInError" class="auth-error">
+                    <p class="auth-error-message">{{signInError}}</p>
+                    <button (click)="signInError=null" class="auth-error-close"></button>
+                </div>
+                <button [disabled]="SignInForm.invalid" class="form-submit-btn" type="submit">Sign In</button>
+            </form>
+            <p>Have no account? <a (click)="authMode='sign up'">Sign up!</a></p>
+        </div>
+        
+        <div [class.active]="authMode === 'sign up'" [class.inactive]="authMode !== 'sign up'" class="auth-form sign-up">
             <p class="form-title">Sign Up</p>
             <form [formGroup]="signUpForm" (ngSubmit)="onSignUp()">
                 <div class="form-group">
@@ -33,27 +56,7 @@ import { FlashcardsService } from '../../services/flashcards.service';
                 </div>
                 <button [disabled]="signUpForm.invalid" class="form-submit-btn" type="submit">Sign Up</button>
             </form>
-        </div>
-        
-        <div class="auth-form sign-in">
-            <p class="form-title">Sign In</p>
-            <form #SignInForm="ngForm" (ngSubmit)="onSignIn(SignInForm)">
-                <div class="form-group">
-                    <input #signInEmail="ngModel" ngModel email required id="sign-in-email" class="form-input" name="email" type="email"/>
-                    <label [class.active]="!!signInEmail.value" class="input-field-label email" for="sign-in-email"></label>
-                    <small [class.active]="signInEmail.invalid && signInEmail.touched" class="input-error">valid email is required</small>
-                </div>
-                <div class="form-group">
-                    <input #signInPwd="ngModel" ngModel required minlength="6" id="sign-in-pwd" class="form-input" name="password" type="password"/>
-                    <label [class.active]="!!signInPwd.value" class="input-field-label password" for="sign-in-pwd"></label>
-                    <small [class.active]="signInPwd.invalid && signInPwd.touched" class="input-error">6 characters password is required</small>
-                </div>
-                <div *ngIf="signInError" class="auth-error">
-                    <p class="auth-error-message">{{signInError}}</p>
-                    <button (click)="signInError=null" class="auth-error-close"></button>
-                </div>
-                <button [disabled]="SignInForm.invalid" class="form-submit-btn" type="submit">Sign In</button>
-            </form>
+            <p>Already a user? <a (click)="authMode='sign in'">Sign in!</a></p>
         </div>
     `,
     styleUrls: ['auth.component.scss']
@@ -62,6 +65,7 @@ export class AuthComponent implements OnInit {
     signUpForm:FormGroup;
     signUpError:string; // error message returned from firebase
     signInError:string; // error message returned from firebase
+    authMode = 'sign up'; // determines which form should be displayed - sign in or sign up
 
     constructor(private firebaseService:FirebaseService, private flashcardsService:FlashcardsService, private router:Router) {}
 
